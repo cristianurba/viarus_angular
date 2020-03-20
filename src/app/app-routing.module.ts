00ad623d33cbc73e3f47b0f9a2f7115cca67d321
Cristian Ariza <cristianurba@gmail.com>
2020-03-20 13:30:09 +0100

    Langing page y routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'landing' },
+  { path: 'landing', component: LandingComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegistroComponent },
+  { path: '**', redirectTo: 'landing' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
